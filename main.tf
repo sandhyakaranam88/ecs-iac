159072c22299a49e4521f1933582372c1cb57b54
Sandhya <111405460+sandhyakaranam88@users.noreply.github.com>
2025-07-29 14:52:04 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,64 @@ provider "aws" {
   region = var.aws_region
 }
 
-module "ecr" {
-  source = "./ecr"
+data "aws_availability_zones" "available" {}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "4.0.2"
+
+  name = "${var.project_name}-vpc"
+  cidr = "10.0.0.0/16"
+  azs  = slice(data.aws_availability_zones.available.names, 0, 2)
+
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.11.0/24", "10.0.12.0/24"]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
 }
 
-module "ecs_cluster" {
-  source = "./ecs/cluster.tf"
+resource "aws_s3_bucket" "uploads" {
+  bucket = "${var.project_name}-uploads-bucket"
+  acl    = "private"
+  versioning {
+    enabled = true
+  }
+}
+
+resource "aws_sqs_queue" "msg_queue" {
+  name = "${var.project_name}-queue"
+}
+
+resource "aws_ecr_repository" "service1" {
+  name = "${var.project_name}-service1"
+}
+
+resource "aws_ecr_repository" "service2" {
+  name = "${var.project_name}-service2"
+}
+
+resource "aws_ecs_cluster" "cluster" {
+  name = "${var.project_name}-cluster"
+}
+
+data "aws_iam_policy_document" "ecs_task_assume" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name               = "${var.project_name}-ecs-exec-role"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_assume.json
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_exec_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
